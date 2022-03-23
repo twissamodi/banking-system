77bdfbf0b4a9cb9a27c7ff87982690d43807002e
Twissa Modi <Twissa_Modi@mckinsey.com>
2022-03-23 22:11:12 +0530

    feat:add NotFoundPage

---
+++ fetch-bank-details/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NotFoundPage.css';
+
+function NotFoundPage() {
+  return (
+    <div className="not-found-container">
+      Error 404
+      <br />
+      Page Not Found
+    </div>
+
+  );
+}
+
+export default NotFoundPage;
