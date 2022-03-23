b393efed4cda1f2719ffd6b20fe51b8995ae3343
Twissa Modi <Twissa_Modi@mckinsey.com>
2022-03-23 23:57:21 +0530

    improv: add backend integration

--- fetch-bank-details/src/pages/HomePage/HomePage.jsx
+++ fetch-bank-details/src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,39 @@
-/* eslint-disable no-nested-ternary */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+/* eslint-disable no-nested-ternary */
+import React, { useState, useEffect } from 'react';
 import BankDetails from '../../components/BankDetails/BankDetails';
 import Dropdown from '../../components/Dropdown/Dropdown';
 import NavBar from '../../components/NavBar/NavBar';
-import {
-  BANK_NAMES, BRANCH_NAMES_AKOLA, BRANCH_NAMES_ABHYUDAYA, BANK_DETAILS,
-} from '../../constants/mockData';
+import { getBankName, getBranchNames, getIFSCCode } from '../../constants/apiEndpoints';
+import makeRequest from '../../utils/makeRequest';
 import './HomePage.css';
 
 function HomePage() {
   const [selectedBank, setSelectedBank] = useState('none');
+  // eslint-disable-next-line no-unused-vars
   const [selectedBranch, setSelectedBranch] = useState('none');
   const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [isBankInitialized, setIsBankInitialized] = useState(false);
+  const [bankNames, setBankNames] = useState([]);
+  const [branchNames, setBranchNames] = useState([]);
+  const [isBranchInitialized, setIsBranchInitialized] = useState(false);
+  const [bankDetails, setBankDetails] = useState({});
+  useEffect(() => {
+    if (!isBankInitialized) {
+      makeRequest(getBankName).then((data) => {
+        setBankNames(data.bankNames);
+        setIsBankInitialized(true);
+      });
+    }
+  }, [isBankInitialized]);
+  useEffect(() => {
+    if (selectedBank !== 'none') {
+      makeRequest(getBranchNames(selectedBank)).then((data) => {
+        setBranchNames(data.bankBranches);
+        setIsBranchInitialized(true);
+      });
+    }
+  }, [selectedBank]);
   const handleBankChange = (event) => {
     setSelectedBank(event.target.value);
   };
@@ -20,20 +41,31 @@ function HomePage() {
     setSelectedBranch(event.target.value);
   };
   const handleClick = () => {
+    makeRequest(getIFSCCode(selectedBank, selectedBranch)).then((data) => {
+      setBankDetails({
+        BANK: selectedBank,
+        BRANCH: selectedBranch,
+        IFSC: data.ifsc,
+      });
+    });
     setIsButtonClicked(true);
   };
-  return (
+  return bankNames.length ? (
     <div className="homepage-content-container">
       <NavBar />
-      {isButtonClicked ? <BankDetails details={BANK_DETAILS} /> : (
+      {isButtonClicked ? <BankDetails details={bankDetails} /> : (
         <div className="container">
-          <Dropdown action="Select bank" options={BANK_NAMES} onChange={handleBankChange} />
-          <Dropdown action="Select branch" options={selectedBank !== 'none' ? selectedBank === 'THE AKOLA DISTRICT CENTRAL COOPERATIVE BANK' ? BRANCH_NAMES_AKOLA : BRANCH_NAMES_ABHYUDAYA : []} onChange={handleBranchChange} />
+          <Dropdown action="Select bank" options={bankNames} onChange={handleBankChange} />
+          {branchNames.length ? <Dropdown action="Select branch" options={branchNames} onChange={handleBranchChange} /> : <div />}
           <button className="dropdown-submit" type="submit" onClick={handleClick}>Submit</button>
         </div>
       )}
-
     </div>
-  );
+  )
+    : (
+      <div>
+        Loading
+      </div>
+    );
 }
 export default HomePage;
